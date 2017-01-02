456ff98f71ed63ac8e7a6406037a86e03746a99a
ww930\a622880 <bhushan.rade@atos.net>
2017-01-02 20:21:35 +0530

    upload files

---
+++ loader.php
@@ -0,0 +1,35 @@
+<?php
+
+$db = mysql_connect("localhost:3306", "root", "") or die("Couldn't Connect");
+
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("MYSQL_USER");
+$dbpwd = getenv("MYSQL_PASSWORD");
+$dbname = getenv("MYSQL_DATABASE");
+
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database");
+	
+	$query = mysql_query("SELECT * from loadtable");
+$values = "";
+while($row = mysql_fetch_assoc($query))
+{
+    $value1 = $row['id'];
+    $value2 = $row['name'];
+    $value3 = $row['number'];
+    $value4 = $row['email'];
+
+    //notice the '.' below.
+    $values .= "<tr><td>".$value1."</td><td>".$value2."</td><td>".$value3."</td><td>".$value4."</td></tr>";
+}
+echo $values;
+	
+}
+$connection->close();
+?>
